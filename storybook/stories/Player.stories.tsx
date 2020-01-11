7f88e13c7e8ebab53c257f4d226e3553465ad9a4
bobinlee <bobeen.lee@huiseoul.com>
2020-01-11 13:16:41 +0900

    [style] player storybook center

--- storybook/stories/Player.stories.tsx
+++ storybook/stories/Player.stories.tsx
@@ -5,16 +5,24 @@ import styled from "styled-components/native";
 
 import MiniAudioPlayer from "src/components/player/MiniAudioPlayer";
 
+const CenterView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 const MiniAudioPlayerView = styled(MiniAudioPlayer)``;
 
-storiesOf("Player", module).add("MiniAudioPlayer", () => {
-  return (
-    <MiniAudioPlayerView
-      size={50}
-      source={{
-        uri:
-          "https://api.soundcloud.com/tracks/736765723/stream?client_id=a281614d7f34dc30b665dfcaa3ed7505"
-      }}
-    />
-  );
-});
+storiesOf("Player", module)
+  .addDecorator((getStory: any) => <CenterView>{getStory()}</CenterView>)
+  .add("MiniAudioPlayer", () => {
+    return (
+      <MiniAudioPlayerView
+        size={50}
+        source={{
+          uri:
+            "https://api.soundcloud.com/tracks/736765723/stream?client_id=a281614d7f34dc30b665dfcaa3ed7505"
+        }}
+      />
+    );
+  });
