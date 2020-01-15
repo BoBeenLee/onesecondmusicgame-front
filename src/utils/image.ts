9a919e846b94b7b62ffa1c25158aa7f73e249626
bobinlee <bobeen.lee@huiseoul.com>
2020-01-16 05:24:29 +0900

    [fix] image utils

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,16 +1,13 @@
 import _ from "lodash";
-import { Image, ImageEditor } from "react-native";
+import { Image } from "react-native";
 
-import { appendQueryParams } from "./uri";
-
-const MAX_IMAGE_WIDTH = 720;
 const IMAGE_MAX_DIMENSION = 1440;
 const IMAGE_MAX_RATIOS = {
   tall: 4 / 3,
   wide: 16 / 9
 };
 
-const getDimensionByRatio = (ratio: string) => {
+export const getDimensionByRatio = (ratio: string) => {
   const [widthRatio, heightRatio] = _.split(ratio, ":");
   return {
     heightRatio,
@@ -18,12 +15,14 @@ const getDimensionByRatio = (ratio: string) => {
   };
 };
 
-const getRatioHeight = (ratio: string, width: number) => {
+export const getRatioHeight = (ratio: string, width: number) => {
   const [widthRatio, heightRatio] = _.split(ratio, ":");
   return (width * Number(heightRatio)) / Number(widthRatio);
 };
 
-const getSize = (uri: string): Promise<{ width: number; height: number }> => {
+export const getSize = (
+  uri: string
+): Promise<{ width: number; height: number }> => {
   const response = new Promise<{ width: number; height: number }>(
     (resolve, reject) => {
       const getSizeSuccessCallback = (width: number, height: number) => {
@@ -38,37 +37,7 @@ const getSize = (uri: string): Promise<{ width: number; height: number }> => {
   return response;
 };
 
-const getResizedUri = (uri: string, width: number = MAX_IMAGE_WIDTH) => {
-  return appendQueryParams(uri, {
-    d: `${width}`
-  });
-};
-
-const cropImage = (
-  uri: string,
-  width: number,
-  height: number,
-  offsetX: number,
-  offsetY: number
-): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    ImageEditor.cropImage(
-      uri,
-      {
-        offset: { x: offsetX, y: offsetY },
-        size: { width, height }
-      },
-      croppedImageURI => {
-        resolve(croppedImageURI);
-      },
-      error => {
-        reject(error);
-      }
-    );
-  });
-};
-
-const getCropDimensions = (width: number, height: number) => {
+export const getCropDimensions = (width: number, height: number) => {
   const maximumWidth = height * IMAGE_MAX_RATIOS.wide;
   const maximumHeight = width * IMAGE_MAX_RATIOS.tall;
 
@@ -84,7 +53,7 @@ const getCropDimensions = (width: number, height: number) => {
   };
 };
 
-const getResizeDimension = (width: number, height: number) => {
+export const getResizeDimension = (width: number, height: number) => {
   const widthReduceRatio = Math.min(IMAGE_MAX_DIMENSION / width, 1);
   const heightReduceRatio = Math.min(IMAGE_MAX_DIMENSION / height, 1);
   const reduceRatio = Math.min(widthReduceRatio, heightReduceRatio);
@@ -93,15 +62,4 @@ const getResizeDimension = (width: number, height: number) => {
     resizeHeight: height * reduceRatio,
     resizeWidth: width * reduceRatio
   };
-};
-
-export {
-  IMAGE_MAX_RATIOS,
-  IMAGE_MAX_DIMENSION,
-  getDimensionByRatio,
-  getRatioHeight,
-  getCropDimensions,
-  getResizeDimension,
-  getResizedUri,
-  getSize
 };
