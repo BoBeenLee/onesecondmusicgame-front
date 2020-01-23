f71d234be1ca1ffad6e0523c74defa2ec709e4dc
bobinlee <bobeen.lee@huiseoul.com>
2020-01-23 16:51:11 +0900

    [refac] move package storybook

---
+++ src/components/index.stories.tsx
@@ -0,0 +1,14 @@
+import "src/components/button/Button.stories";
+import "src/components/text/Text.stories";
+import "src/components/card/Card.stories";
+import "src/components/player/Player.stories";
+import "src/components/topbar/TopBar.stories";
+import "src/components/carousel/Carousel.stories";
+import "src/components/webview/Webview.stories";
+import "src/components/popup/Popup.stories";
+import "src/components/icon/Icon.stories";
+import "src/components/input/Input.stories";
+import "src/components/backdrop/BackDrop.stories";
+import "src/components/image/Image.stories";
+import "src/components/progress/Progress.stories";
+import "src/components/tab/Tab.stories";
