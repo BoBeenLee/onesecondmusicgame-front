de53b73098c06e501ac8adf58b23030e17bdedc2
bobinlee <bobeen.lee@huiseoul.com>
2020-02-20 21:07:31 +0900

    [style] 이미지 적용

--- src/screens/game/GamePlayScreen.tsx
+++ src/screens/game/GamePlayScreen.tsx
@@ -185,6 +185,26 @@ const AnswerStatus = styled.Image`
   height: 225px;
 `;
 
+const AnswerStatusArtworkView = styled.View`
+  width: 227px;
+  height: 225px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AnswerStatusArtworkBG = styled.Image`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+`;
+
+const AnswerStatusArtwork = styled.Image`
+  width: 225px;
+  height: 225px;
+`;
+
 const AnswerView = styled.View`
   flex: 1;
   align-items: center;
@@ -633,6 +653,7 @@ class GamePlayScreen extends Component<IProps, IStates> {
     const { songAnswerSeconds } = this.state;
     const isAnswer = Boolean(currentGameHighlight?.isUserAnswer);
     const userAnswer = currentGameHighlight?.userAnswer ?? "";
+
     return (
       <AnswerContainer>
         {isAnswer && !_.isEmpty(userAnswer) ? (
@@ -648,11 +669,7 @@ class GamePlayScreen extends Component<IProps, IStates> {
           <GameStopButton source={images.pauseButton} onPress={this.exit} />
         </Header>
         <AnswerContent>
-          {isAnswer ? (
-            <AnswerStatus source={images.correctCD} />
-          ) : (
-            <AnswerStatus source={images.wrongCD} />
-          )}
+          {this.renderAnswerStatus}
           <AnswerView>
             {isAnswer ? (
               !_.isEmpty(userAnswer) ? (
@@ -701,6 +718,36 @@ class GamePlayScreen extends Component<IProps, IStates> {
         </Footer>
       </AnswerContainer>
     );
+  }
+
+  private get renderAnswerStatus() {
+    const { currentGameHighlight } = this.gamePlayHighlights;
+    const isAnswer = Boolean(currentGameHighlight?.isUserAnswer);
+
+    if (isAnswer) {
+      if (currentGameHighlight?.artworkUrl) {
+        return (
+          <AnswerStatusArtworkView>
+            <AnswerStatusArtwork
+              source={{ uri: currentGameHighlight?.artworkUrl }}
+            />
+            <AnswerStatusArtworkBG source={images.correctArtworkCD} />
+          </AnswerStatusArtworkView>
+        );
+      }
+      return <AnswerStatus source={images.correctCD} />;
+    }
+    if (currentGameHighlight?.artworkUrl) {
+      return (
+        <AnswerStatusArtworkView>
+          <AnswerStatusArtwork
+            source={{ uri: currentGameHighlight?.artworkUrl }}
+          />
+          <AnswerStatusArtworkBG source={images.wrongArtworkCD} />
+        </AnswerStatusArtworkView>
+      );
+    }
+    return <AnswerStatus source={images.wrongCD} />;
   }
 
   private onSongAnswerChangeText = (text: string) => {
@@ -830,7 +877,6 @@ class GamePlayScreen extends Component<IProps, IStates> {
       artist: "???",
       artwork: artworkUrl
     });
-    // await this.onPlayItem(currentGameHighlight);
   };
 
   private onFinishPopup = async () => {
