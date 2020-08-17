ef92d90631d186b81335a52ecc62b421213035ca
BoBeenLee <globaldev@naver.com>
2020-08-17 20:34:20 +0900

    fix: fixed sentry

--- src/configs/sentry.ts
+++ src/configs/sentry.ts
@@ -3,7 +3,8 @@ import env from "src/configs/env";
 
 export const initialize = () => {
   Sentry.init({
-    dsn: "https://fe73c0e544da47a48fa4900dbedf7717@sentry.io/1885677",
+    dsn:
+      "https://fe73c0e544da47a48fa4900dbedf7717@o342480.ingest.sentry.io/1885677",
     environment: env.REACT_ENV
   });
 };
